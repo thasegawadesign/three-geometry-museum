8c40b905b64feb8b524e179de538015d3a5809fd
thasegawadesign <hasegawatatsuya206@gmail.com>
2025-12-12 21:13:58 +0900

    add: test mesh

--- src/main.js
+++ src/main.js
@@ -1,1 +1,44 @@
+import * as THREE from 'three';
 import './style.css';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+
+console.log(THREE);
+
+const canvas = document.getElementById('webgl');
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+camera.position.z = 5;
+scene.add(camera);
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.autoRotate = true;
+controls.autoRotateSpeed = 2.0;
+controls.enablePan = false;
+
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshBasicMaterial({
+  color: 0x00ff00,
+  wireframe: true,
+});
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+const tick = () => {
+  controls.update();
+  renderer.render(scene, camera);
+  requestAnimationFrame(tick);
+};
+tick();
