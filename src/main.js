32bed4b8019e99aa47f13929ca7f521d197f431f
thasegawadesign <hasegawatatsuya206@gmail.com>
2025-12-12 21:27:11 +0900

    add: resize event

--- src/main.js
+++ src/main.js
@@ -2,9 +2,23 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import './style.css';
 
-console.log(THREE);
-
 const canvas = document.getElementById('webgl');
+
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+window.addEventListener('resize', () => {
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
 
 const scene = new THREE.Scene();
 
